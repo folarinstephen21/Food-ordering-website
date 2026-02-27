11c0329c111aa98505a941bdb007e4ea56a966e8
folarinstephen21 <249096587+folarinstephen21@users.noreply.github.com>
2026-02-27 13:57:09 +0100

    Initial commit, organized roites and started work on header and explore sections

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
+import Home from './Pages/Home/Home'
+import Cart from './Pages/Cart/Cart'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}  />
+        <Route path='/cart' element={<Cart />}  />
+        <Route path='/order' element={<PlaceOrder />}  />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
