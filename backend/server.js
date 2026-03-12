9616e27f8cfe7c1e2bf30d1553328f8c11d69dd8
folarinstephen21 <249096587+folarinstephen21@users.noreply.github.com>
2026-03-12 16:25:14 +0100

    minor

--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${port}`);
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
